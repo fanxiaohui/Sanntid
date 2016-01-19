7a75d72f70accdbce1d194ff2706304f4cc906c2
prestegaard <hakonp@gmail.com>
2016-01-19 11:56:45 +0100

    Task one from week 2

---
+++ Excercise1/task1.py
@@ -0,0 +1,33 @@
+# Python 3.3.3 and 2.7.6
+# python helloworld_python.py
+
+from threading import Thread
+
+i = 0
+
+def thread1_func():
+	global i
+	print("Hello from a thread1!")
+	for j in range (0,1000000):
+   		i +=1
+   	print("i =+ %d" % i)
+
+def thread2_func():
+	global i
+	print("Hello from a thread2!")
+	for k in range (0,1000000):
+		i -=1
+	print("i -= %d" % i) 
+
+
+def main():
+    thread1 = Thread(target = thread1_func, args = (),)
+    thread2 = Thread(target = thread2_func, args = (),)
+    thread1.start()
+    thread2.start()
+    thread1.join()
+    thread2.join()
+    print("Hello from main!")
+    print("i = %d" % i) 
+   
+main()
