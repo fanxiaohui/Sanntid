5b00f4021db5609eac0d6f13b88e7b7beda1f6a7
lovvig <vegard_b_lovvig@hotmail.com>
2016-04-01 14:03:06 +0200

    Update network_module.c

--- Project/network/network_module.c
+++ Project/network/network_module.c
@@ -1,3 +1,6 @@
+
+
+
 #include <stdio.h>
 #include <string.h>    //strlen
 #include <stdlib.h>    //strlen
@@ -23,36 +26,30 @@
 #include <semaphore.h>
 #include <assert.h>
 #include <stdlib.h>
-
+#include "main.h"
 //#include "lift.h"
 #include "elev.h"
 
 #define PORT 20017
 #define PORTTCP 20017
 #define SERVER "129.241.187.255"
-#define BUFSIZE 1024
 
 //Message struct
-struct message {
+struct Message {
   int id;             //Message info
   int elev;           //Which elevator(s) gets the message
-  char message[1500]; //Message
+  struct Elevator //Message
 };
 
 struct Elevator {
+   int type;
    int floor_current;
    int destination;
    int reached_destination;
   // tag_elev_motor_direction direction;
    int new_floor_order;
+   int button_matrix[4][3];
 }elevator;
-struct Message_received {
-  int ID;
-  int type;
-  int new_floor_order;
-  int floor_indicator[4][3];
-};
-
 
 
 char* BROADCASTIP;
@@ -63,6 +60,12 @@ char* serv_ip;
 int optval = 1;
 int clients = 0;
 int flag = 0;
+int connection = 1;
+*new_sock;
+int broadcast = 0;
+int sockets[2];
+
+//Lagre sockets i et array, itterer over for å oppnå multicast 
 
 pthread_mutex_t mutex;
 // gcc -std=gnu11 -Wall -g -o udp udp_send_recv.c -lpthread
@@ -75,23 +78,34 @@ void *tcp_connection_handler(void *socket_desc) //Connection handler for TCP con
     //Get the socket descriptor
     int sock = *(int*)socket_desc;
     int read_size;
-    char *message , client_message[4], elev_go[2000], elev_go2[2000];
-
+    char *message , client_message[500], elev_go[2000], elev_go2[2000], halla[500];
+    bzero(halla,500); 
+    strcpy(halla,"halla");
     strcpy(elev_go,"elev go");
     strcpy(elev_go2,"elevatorgo");
     //Send some messages to the client
     //message = "Greetings! I am your connection handler\n";
     //write(sock , message , strlen(message));
      
-    bzero(client_message,4); 
+    bzero(client_message,500); 
     //Receive a message from client
-    while( (read_size = recv(sock , client_message , 4 , 0)) > 0 )
+    while( (read_size = recv(sock , client_message , 500 , 0)) > 0 )
     {
+        if(atoi(client_message)==1){
+        	for(int i = 0;i<clients;i++){
+        		int tempsock = *(int*)sockets[i];
+        		write(tempsock, client_message, strlen(client_message));
+        	}
+        }
+        
         //Send the message back to client
         write(sock , client_message , strlen(client_message));
         elevator.new_floor_order = atoi(client_message);
-      
-        printf("Client message: %d", elevator.new_floor_order);
+      	
+      	//puts("Client message:");
+      	//puts(client_message);
+      	printf("Client message: %s\n", client_message);
+        //printf("Client message: %d", elevator.new_floor_order);
 
        /* if(!strcmp(client_message,elev_go));
         {
@@ -101,7 +115,7 @@ void *tcp_connection_handler(void *socket_desc) //Connection handler for TCP con
             printf("Halla");
         }
         */
-        bzero(client_message,4);
+        bzero(client_message,500);
     }
      
     if(read_size == 0)
@@ -167,7 +181,7 @@ void tcp_listen(){
         pthread_t sniffer_thread;
         new_sock = malloc(1);
         *new_sock = new_socket;
-         
+        sockets[clients-1] = new_socket; 
         if( pthread_create( &sniffer_thread , NULL ,  tcp_connection_handler , (void*) new_sock) < 0)
         {
             perror("could not create thread");
@@ -280,9 +294,28 @@ void connection_init(void){
           boss = 0;
           serv_ip = inet_ntoa(sa.sin_addr);
           printf("Master discovered, i'm slave :(\n");
+          connection = 1;
        }
        else
+       {
           printf("Master not discovered\n");
+          if(connection == 0)
+          {
+          	boss = 1;
+          	printf("Boss: %d",boss);
+          	pthread_t tcp_listen_thread,udp_listen_thread,udp_send_thread,communication_thread,elev_thread;
+		    pthread_create( &udp_listen_thread, NULL, udp_listen, NULL);
+		    pthread_create( &udp_send_thread, NULL, udp_send, NULL);
+		    pthread_create( &tcp_listen_thread, NULL, tcp_listen, NULL);
+		    pthread_create( &elev_thread, NULL, elevator_thread, NULL);
+   	        // pthread_create( &communication_thread, NULL, communication, NULL);
+            pthread_join( udp_listen_thread , NULL);
+		    pthread_join( udp_send_thread , NULL);
+		    pthread_join( tcp_listen_thread , NULL);
+	        pthread_join( elev_thread, NULL);
+		    //pthread_join( communication_thread, NULL);
+          }
+       }
       //printf("recsize: %zu\n ", recsize);
       //sleep(0.1);
       //printf("datagram: %.*s\n", (int)recsize, buffer);
@@ -347,7 +380,7 @@ void* udp_send(){
   char buffer[200];
   strcpy(buffer, "Halla");
 
-//Get broadcast address
+  //Get broadcast address
   int fd;
   struct ifreq ifr;
   fd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -359,7 +392,7 @@ void* udp_send(){
   close(fd);
   char* BROADCASTIP = inet_ntoa(((struct sockaddr_in *)&ifr.ifr_broadaddr)->sin_addr);
 
-//Set up socket
+  //Set up socket
   sock = socket(AF_INET, SOCK_DGRAM, 0);
   int broadcastEnable=1;
   int ret=setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &broadcastEnable, sizeof(broadcastEnable));
@@ -411,6 +444,45 @@ void* udp_send(){
   return NULL; //Broadcasts on UDP
 }
 
+void *tcp_send(void *socket_desc){
+	//Get the socket descriptor
+    int sock = *(int*)socket_desc;
+    char buf[BUFSIZE];
+    while(connection){
+      printf("Please enter msg: \n");
+      bzero(buf, BUFSIZE);
+      fgets(buf, BUFSIZE, stdin);   
+      //send the message line to the server 
+      write(sock, buf, strlen(buf));
+    }
+    free(socket_desc);
+     
+}
+
+void tcp_recieve(void *socket_desc){
+  //Get the socket descriptor
+  int sock = *(int*)socket_desc;
+  char buf[BUFSIZE];
+  char empty[BUFSIZE];
+  bzero(empty, BUFSIZE);
+  bzero(buf, BUFSIZE);
+  //strcpy(empty," ");
+  while(connection){
+    
+    read(sock, buf , BUFSIZE);
+    if(!strcmp(buf,empty)){
+        printf("recv failed\n");
+        connection = 0;
+      }
+    else{
+      printf("Server message: %s\n", buf);
+    }
+    bzero(buf, BUFSIZE);
+  }
+  //free(socket_desc);
+     
+}
+
 void client_init(){
     int sockfd, portno, n;
     struct sockaddr_in serveraddr;
@@ -428,7 +500,8 @@ void client_init(){
     if (sockfd < 0) 
         error("ERROR opening socket");
 
-     //gethostbyname: get the server's DNS entry 
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval));
+    //gethostbyname: get the server's DNS entry 
     server = gethostbyname(hostname);
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host as %s\n", hostname);
@@ -454,7 +527,7 @@ void client_init(){
       printf("%s\n", server_reply);
     }
 
-    for(;;){
+    /*for(;;){
 
       printf("Please enter msg: ");
       fgets(buf, BUFSIZE, stdin); 
@@ -468,43 +541,28 @@ void client_init(){
       printf("Server reply: %s\n", server_reply);
       
     }
-    //pthread_t client_thread1,client_thread2;
-    //pthread_create(&client_thread1, NULL, tcp_send, NULL);
-    //pthread_create(&client_thread2, NULL, tcp_recieve, NULL);
-    //pthread_join(client_thread1, NULL);
-    //pthread_join(client_thread2, NULL);
-  
-    close(sockfd); //Makes TCP connection to server
+    */
+    //Client threads
+	//pthread_create( &sniffer_thread , NULL ,  tcp_connection_handler , (void*) new_sock) < 0)
+    //void *tcp_connection_handler(void *socket_desc) //Connection handler for TCP connections
+	//{
+    //Get the socket descriptor
+    //int sock = *(int*)socket_desc;
+    puts("yolo");
+    pthread_t client_thread1,client_thread2;
+
+    new_sock = malloc(1);
+    *new_sock = sockfd;
+
+    pthread_create(&client_thread1, NULL, tcp_send, (void*) new_sock);
+    pthread_create(&client_thread2, NULL, tcp_recieve, (void*) new_sock);
+    pthread_join(client_thread1, NULL);
+    pthread_join(client_thread2, NULL);
+    close(sockfd); //Makes TCP connection to server,
+    connection_init();
 }
 
-/*
-void* tcp_send(){
-  int n,sockfd;
-    char buf[BUFSIZE];
-    while(1){
-      printf("Please enter msg: ");
-      bzero(buf, BUFSIZE);
-      fgets(buf, BUFSIZE, stdin);   
-      //send the message line to the server 
-      n = write(sockfd, buf, strlen(buf));
-    if (n < 0) 
-      error("ERROR writing to socket");
-    }
-}
 
-void* tcp_recieve(){
-  int n,sockfd;
-  char buf[BUFSIZE];
-  while(1){
-    bzero(buf, BUFSIZE);
-    n = read(sockfd, buf, BUFSIZE);
-    if (n < 0) 
-      error("ERROR reading from socket");
-    printf("Echo from server: %s", buf);
-  
-  }
-}
-*/
 
 
 //----------------------------
@@ -587,6 +645,7 @@ void * buttons(){
 */
 //-------------------------------------
 
+
 int main(int argc , char *argv[])
 {   
   
@@ -619,3 +678,7 @@ int main(int argc , char *argv[])
      
     return 0;
 }
+
+
+
+
