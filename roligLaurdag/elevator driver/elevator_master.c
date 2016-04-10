c8ed40b2bb9ee79a2a30c1632a8453b5b76d2953
prestegaard <hakonp@gmail.com>
2016-04-10 11:53:25 +0200

    Updated,

--- roligLaurdag/elevator driver/elevator_master.c
+++ roligLaurdag/elevator driver/elevator_master.c
@@ -10,7 +10,7 @@
 //The big cost function // ------------------
 
 
-int queue[6][1];
+int queue[6];
 
 
 switch(messeage_receive.type){
@@ -28,7 +28,6 @@ switch(messeage_receive.type){
 		}
 		
 }
-esseage_receive
 int add_to_queue(&elev_button_type_t type, & int  floor){
 	if(type == BUTTON_CALL_UP && queue[floor]){
 		return 0; 
@@ -39,27 +38,6 @@ int add_to_queue(&elev_button_type_t type, & int  floor){
 	else
 		return 1;
 }
-cost_function(&messeage_receive);
-Message cost_function(&Message messeage_receive){
-	//If already in queue, exit
-	//If not in queue, add it, designate it to an elevator and update the queue list
-
-	//BUTTON_PRESS INSIDE: If this new floor is in the queue list and the same direction, designate it to this elevator
-
-	for(int i =0;i<6;i++){
-		if(messeage_receive.queue[i])
-	}
-
-}
-
-
-
-
-
-//if elevator inside is going to outside ordered floor -> assing it
-//if elevator is free -> assign it
-vector Elevator elev_client;
-elev_client.pushback
 
 struct Elevator elev_client[3];
 
@@ -129,15 +107,4 @@ int cost_function(& Message message_recv){
 	// -> I assign client 1
 	client =1;
 	return client;
-}
-
-
-
-//recieve BUTTON PRESS OUTSIDE:
-
-int add_to_queue(){
-	int temp =	messeage_receive.button.floor + messeage_receive.button.type
-
-
-	return if 
 }
