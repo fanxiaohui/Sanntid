5fc2c2387df3980d230456a1953053ef5505945f
prestegaard <hakonp@gmail.com>
2016-04-17 16:07:51 +0200

    Update lift.c
    Cleaned up to code, made some changes in the the elevator thread reguarding always stoping at top and bottom floor

--- project/lift.c
+++ project/lift.c
@@ -1,28 +1,40 @@
 #include "lift.h"
+#include "elev.h"
+#include "communication.h"
 #include <pthread.h>
 #include <semaphore.h>
 #include <unistd.h>
-#include "elev.h"
-#include "main.h"
-#include "communication.h"
-//SLAVE:
-//Shared variables
-/*
-struct Message{
-	int ID; 
-	int type;
-	Elevator elevator;
-	Button button;
-	int lamps_outside[6];	
-};
-*/
+
+
+
+#ifdef __WIN32__
+# include <winsock2.h>
+#else
+# include <sys/socket.h>
+#endif
+#include <unistd.h>    
+#include <arpa/inet.h> 
+#include <pthread.h> 
+#include <limits.h>
+#include <ifaddrs.h>
+#define _GNU_SOURCE 
+#include <netdb.h>
+#include <linux/if_link.h>
+#include <strings.h>
+#include <net/if.h>
+#include <netinet/in.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
 
 
 Elevator elevator;
-//Button button;
-pthread_mutex_t mutex;
 int elev_orders[6][2];
-//RECIEVING THREAD
+int master_checkin = 0;
+connection;
+network;
+pthread_mutex_t mutex;
+
+
 void* client_message_handler(void *msg_recv){
 	puts("Client message handler started");
 	pthread_mutex_init(&mutex,NULL);
@@ -32,13 +44,7 @@ void* client_message_handler(void *msg_recv){
 	for(int i = 0;i<BUFSIZE;i++){
 		receive[i] = buf[i];
 	}
-	/*
-	for(int i = 0;i<19;i++){
-		printf("Recieved: %d \n", receive[i]);
-	}
-	*/
 	Message message_receive = deserialization(receive);
-
 	switch(message_receive.type){
 		case ELEV_ORDER:
 			puts("ELEV ORDER");
@@ -55,6 +61,10 @@ void* client_message_handler(void *msg_recv){
 				queue[i][0] = message_receive.orders[i][0];
 				queue[i][1] = message_receive.orders[i][1];
 			}
+			puts("\nNew queue");
+        	for(int i = 0;i<6;i++){
+          		printf("Order: %d \t ID: %d\n",queue[i][0],queue[i][1]);
+       		 }
 
 			printf("Message_recieve.ID: %d\n", message_receive.ID);
 			printf("My ID: %d\n", my_ID);
@@ -70,6 +80,7 @@ void* client_message_handler(void *msg_recv){
 		
 			break;
 		case ORDER_UPDATE:
+			puts("ORDER UPDATE");
 			for(int i=0;i<6;i++){ //Global queue			 
 				elev_orders[i][0] = message_receive.orders[i][0];
 				elev_orders[i][1] = message_receive.orders[i][1];
@@ -85,7 +96,7 @@ void* client_message_handler(void *msg_recv){
 				}
 			}
 			printf("Elev queue: ");
-			for(int i = 0;i<4;i++){
+			for(int i = 0;i<N_FLOORS;i++){
 				printf("%d ",elevator.queue[i]);
 			}
 			printf("\n");
@@ -93,82 +104,135 @@ void* client_message_handler(void *msg_recv){
 			break;
 		case BUTTON_LAMP:
 				puts("BUTTON LAMP");
-				//for(int i = 0;i<6;i++){
-					//printf("%d ",message_receive.lamps_outside[i]);
-				//}
-			    elev_set_button_lamp(BUTTON_CALL_UP, 0, message_receive.orders[0][0]);
+				elev_set_button_lamp(BUTTON_CALL_UP, 0, message_receive.orders[0][0]);
 				elev_set_button_lamp(BUTTON_CALL_UP, 1, message_receive.orders[1][0]);
 				elev_set_button_lamp(BUTTON_CALL_UP, 2, message_receive.orders[2][0]);
 				elev_set_button_lamp(BUTTON_CALL_DOWN, 1, message_receive.orders[3][0]);
 				elev_set_button_lamp(BUTTON_CALL_DOWN, 2, message_receive.orders[4][0]);
 				elev_set_button_lamp(BUTTON_CALL_DOWN, 3, message_receive.orders[5][0]);
-			break;
+				break;
 		case ID_UPDATE:
+			puts("ID UPDATE");
 			my_ID = message_receive.ID;
+			char id_upd[BUFSIZE];
+			Message message_send_ID;
+			message_send_ID.ID = my_ID;
+			serialization(ID_UPDATE, message_send_ID, id_upd);
+			pthread_t message_send_thread_ID;
+			pthread_create(&message_send_thread_ID, NULL , tcp_send, (void*)id_upd);
 			break;
-		case BACKUP:
+		case NETCHECK:
+			puts("NETCHECK");
+			master_checkin = 1;
+			char net_check[BUFSIZE];
+			Message message_send;
+			serialization(NETCHECK, message_send, net_check);
+			pthread_t message_send_thread;
+			pthread_create(&message_send_thread, NULL , tcp_send, (void*)net_check);
+			sleep(6);
+			master_checkin = 0;
+			sleep(5);
+			if(master_checkin == 0){
+				puts("Master disconnected\n");
+				connection = 0;
+			}
 			break;
-
 	}
-
 }
 				
 void elevator_thread(){
+	int sum=0;
 	Message message_send;
-	//elev_init();
-	elevator.floor_current = 0;
-	elevator.direction=DIRN_STOP;
-	int last_floor =0;
-	elev_motor_direction_t  next_direction=DIRN_UP;
+ 	elevator.queue[0] = 0;
+	elevator.queue[1] = 0;
+	elevator.queue[2] = 0;
+	elevator.queue[3] = 0;
+ 	FILE *fp;
+   	char buff[255];
+
+   fp = fopen("local_order_queue.txt", "a+");
+	for(int i=0;i<N_FLOORS;i++){
+	   fscanf(fp, "%s", buff);
+	   printf("%d : %s\n", i, buff );
+	   if(buff[0] == '1'){
+		   elevator.queue[i]=1;
+		   sum++;
+		}
+		else
+			elevator.queue[i]=0;
+   	}
+   fclose(fp);
+	
+	if(!sum){
+		elev_init();
+		elevator.floor_current = 0;
+		elevator.direction=DIRN_STOP;
+	}
 	message_send.elevator.direction = elevator.direction;
 	message_send.elevator.floor_current = elevator.floor_current;
+	
 	char transmit[BUFSIZE];
 	serialization(ELEV_UPDATE, message_send, transmit);
 	pthread_t message_send_thread;
 	pthread_create(&message_send_thread, NULL , tcp_send, (void*)transmit);
+	
 	int floor_update_not_sent=1;
-	
-	//elevator.new_floor_order=-1;
-	elevator.reached_destination=-1;
-	
-	int sum=0;
 	while(1){
 		if(sum != 0){
-			//puts("New order, elev thread");
 			elevator.floor_current = elev_get_floor_sensor_signal();
-			if(next_direction==DIRN_UP){
-				for(int i=elevator.floor_current;i<N_FLOORS;i++){
-					if(elevator.queue[i]){
-						elevator.direction=DIRN_UP;
+			int orders_up=0;
+			int orders_down=0;
+			if(elevator.direction == DIRN_STOP){
+				for(int i=elevator.floor_current+1;i<N_FLOORS;i++){
+					if(elevator.queue[i] == 1){
+						orders_up++;
+						puts("Order up");
+					}
+
+				}
+				for( int i=elevator.floor_current-1;i>=0;i--){
+					if(elevator.queue[i] == 1){
+						orders_down++;
+						puts("Order down");
 					}
 				}
-			}
-			else if(next_direction==DIRN_DOWN){
-				for(int i = elevator.floor_current;i>=0;i--){
-					elevator.direction==DIRN_DOWN;
+				if (orders_up == 0 && orders_down == 0){
+				elevator.direction=DIRN_STOP;
+				}
+				else if(orders_up>orders_down){
+					elevator.direction=DIRN_UP;
+					puts("DIRN UP");
+				}
+			
+				else{
+					elevator.direction=DIRN_DOWN;
+					puts("DIRN DOWN");
 				}
 			}
-			if(elevator.direction!=DIRN_STOP){
-				message_send.elevator.direction = elevator.direction;
-				message_send.elevator.floor_current = elevator.floor_current;
-				char transmit[BUFSIZE];
-				serialization(ELEV_UPDATE, message_send, transmit);
-				pthread_t message_send_thread;
-	    		pthread_create(&message_send_thread, NULL , tcp_send, (void*)transmit);	
-				elev_set_motor_direction(elevator.direction);
-		}
+			for(int i=0;i<N_FLOORS;i++){
+				message_send.elevator.queue[i]=elevator.queue[i];
+			}
+			message_send.elevator.direction = elevator.direction;
+			message_send.elevator.floor_current = elevator.floor_current;
+			char transmit[BUFSIZE];
+			serialization(ELEV_UPDATE, message_send, transmit);
+			pthread_t message_send_thread;
+			pthread_create(&message_send_thread, NULL , tcp_send, (void*)transmit);	
 			
+			elev_set_motor_direction(elevator.direction);		
 		}
 
 		while(sum){
 			elevator.floor_current=elev_get_floor_sensor_signal();
 			if(elevator.floor_current!=-1){
 				elev_set_floor_indicator(elevator.floor_current);
+				if((elevator.floor_current==0 && elevator.direction==DIRN_DOWN) || (elevator.floor_current==N_FLOORS&&elevator.direction==DIRN__UP)){
+					elevator.direction=DIRN_STOP;
+					elev_set_motor_direction(elevator.direction);
+				}
 				if(elevator.queue[elevator.floor_current]==1){
-					//stop
 					elev_set_motor_direction(DIRN_STOP);
 					elev_set_button_lamp(BUTTON_INSIDE,elevator.floor_current,0);
-
 					elevator.queue[elevator.floor_current]=0;
 					printf("Elevator queue updated: \t");
 					sum = 0;
@@ -177,18 +241,14 @@ void elevator_thread(){
 						sum+=elevator.queue[i];
 						printf("%d ", elevator.queue[i]);		
 					}
-					
-					
-					if(sum==0){
-						puts("\nDIRN STOP");
-						elevator.direction = DIRN_STOP;
-						next_direction= DIRN_STOP;
-					}
-					else{
-						elevator.direction=get_next_direction(elevator, last_floor, &next_direction);
-					}
 
+					FILE *fp;
+			   		fp = fopen("local_order_queue.txt", "w+");
+			   		fprintf(fp,"%d %d %d %d", elevator.queue[0], elevator.queue[1], elevator.queue[2], elevator.queue[3] );
+			   		fclose(fp);
+					
 					message_send.elevator.prev_direction = elevator.direction;	
+					elevator.direction = DIRN_STOP;
 					message_send.elevator.direction = elevator.direction;
 					message_send.elevator.floor_current = elevator.floor_current;
 					printf("Floor current: %d\n", message_send.elevator.floor_current);
@@ -197,57 +257,97 @@ void elevator_thread(){
 						message_send.orders[i][0] = elev_orders[i][0];
 						message_send.orders[i][1] = elev_orders[i][1];
 					}
-
 					switch (elevator.floor_current){
 						case 0:
 							message_send.orders[0][0] = 0;
 							message_send.orders[0][1] = my_ID;
+							elevator.next_direction = DIRN_UP;
 							break;
 						case 1:
 							if(elev_orders[1][1] == my_ID){
 								message_send.orders[1][0] = 0;
 								message_send.orders[1][1] = my_ID;
+								elevator.next_direction = DIRN_UP;
 							}
 							else {
 								message_send.orders[3][0] = 0;
 								message_send.orders[3][1] = my_ID;
+								elevator.next_direction = DIRN_DOWN;
 							}
-							//puts("Case 1");
 							break;
 						case 2:
 							if(elev_orders[2][1] == my_ID){
 								message_send.orders[2][0] = 0;
 								message_send.orders[2][1] = my_ID;
+								elevator.next_direction = DIRN_UP;
 							}
 							else {
 								message_send.orders[4][0] = 0;
 								message_send.orders[4][1] = my_ID;
+								elevator.next_direction = DIRN_DOWN;
 							}
-							//puts("Case 2");
 							break;							
 						case 3:
 							message_send.orders[5][0] = 0;
 							message_send.orders[5][1] = my_ID;
+							elevator.next_direction = DIRN_DOWN;
 							break;
 					}
 					puts("ORDER UPDATE");
-       				 for(int i = 0;i<6;i++){
+       				for(int i = 0;i<6;i++){
           				printf("Order: %d \t ID: %d\n",message_send.orders[i][0], message_send.orders[i][1]);
-
-        				}
-					send_message(ORDER_UPDATE,message_send);
+        			}
+					
+					char transmit[BUFSIZE];
+					serialization(ORDER_UPDATE, message_send, transmit);
+					pthread_t message_send_thread;
+					pthread_create(&message_send_thread, NULL , tcp_send, (void*)transmit);	
+					
 					floor_update_not_sent=0;
 					elev_set_door_open_lamp(1);
 				    elev_set_button_lamp(BUTTON_INSIDE,elevator.floor_current,0);
 				    sleep(2);
 				    elev_set_door_open_lamp(0);
-				    last_floor=elevator.floor_current;
+				    sum = 0;
+				    switch (elevator.next_direction){
+				    	case DIRN_DOWN:
+				    		for(int i = elevator.floor_current;i>=0;i--){
+				    			sum += elevator.queue[i];
+				    		}
+				    		if(sum!=0){
+				    			elevator.direction = DIRN_DOWN;
+				    		}
+				    		else {
+				    			for(int i = elevator.floor_current;i<N_FLOORS;i++){
+				    				sum+=elevator.queue[i];
+				    			}
+				    			if(sum!=0)
+				    				elevator.direction = DIRN_UP;
+				    		}
+				    		break;
+				    	case DIRN_UP:
+				    		for(int i = elevator.floor_current;i<N_FLOORS;i++){
+				    			sum += elevator.queue[i];
+				    		}
+				    		if(sum!=0){
+				    			elevator.direction = DIRN_UP;
+				    		}
+				    		else {
+				    			for(int i = elevator.floor_current;i>=0;i--){
+				    				sum+=elevator.queue[i];
+				    			}
+				    			if(sum!=0)
+				    				elevator.direction = DIRN_DOWN;
+				    		}
+				    		break;
+				    }
 				    elev_set_motor_direction(elevator.direction);
 				}
 				else if(floor_update_not_sent){
 					message_send.elevator.direction = elevator.direction;
 					message_send.elevator.prev_direction = elevator.direction;
 					message_send.elevator.floor_current = elevator.floor_current;
+					
 					char transmit[BUFSIZE];
 					serialization(ELEV_UPDATE, message_send, transmit);
 					pthread_t message_send_thread;
@@ -274,10 +374,10 @@ void button_check(){
 	elev_button_type_t button_type;
 	int floor;
 	int button_is_pressed=0;
-	puts("Button check");
+	puts("Button check thread started");
 	while (1){
-		for(floor = 0;floor<N_FLOORS;floor++){ 	//floor
-			for(button_type = 0;button_type<N_BUTTONS;button_type++){ //button_type
+		for(floor = 0;floor<N_FLOORS;floor++){ 
+			for(button_type = 0;button_type<N_BUTTONS;button_type++){ 
 				if(elev_get_button_signal(button_type,floor)==1){
 					if(button_type==BUTTON_INSIDE){ 
 						elev_set_button_lamp(BUTTON_INSIDE,floor,1);
@@ -285,13 +385,11 @@ void button_check(){
 						}
 					pthread_mutex_lock(&mutex);
 					button_is_pressed=1;
-					//sleep(1);
 					message_send.button.floor=floor;
 					message_send.button.type=button_type;
 					pthread_mutex_unlock(&mutex);
 					while(elev_get_button_signal(button_type,floor));
 					break;
-					//printf("Button: Floor: %d button_type: %d\n",floor,button_type);
 				}
 			}
 			if(button_is_pressed)
@@ -300,16 +398,17 @@ void button_check(){
 		if(button_is_pressed){ 
 			
 			puts("Button is pressed, creating thread\n");
-			//while(elev_get_button_signal(button_type,floor));
 			button_is_pressed=0;
-			//message_send.type = BUTTON_CHECK;
-			//SPAWN THREAD TO DO THE SENNDING
-		/*	char transmit[BUFSIZE];
+			char transmit[BUFSIZE];
 			serialization(BUTTON_CHECK, message_send, transmit);
 			pthread_t message_send_thread;
     		pthread_create(&message_send_thread, NULL , tcp_send, (void*)transmit);	
-		*/
-    		send_message(BUTTON_CHECK, message_send);
+			if(message_send.button.type==BUTTON_INSIDE){
+				FILE *fp;
+			   fp = fopen("local_order_queue.txt", "w+");
+			   fprintf(fp,"%d %d %d %d", elevator.queue[0], elevator.queue[1], elevator.queue[2], elevator.queue[3] );
+			   fclose(fp);
+    		}
 		}
 		if(!connection){
 			break;
@@ -318,81 +417,77 @@ void button_check(){
 	} 			
 }
 
-elev_motor_direction_t get_next_direction(Elevator elevator, int last_floor, elev_motor_direction_t *direction_next){
-					int up, down,delta;
-					switch(elevator.floor_current){
-						case 3:
-								for(int i=3;i>=0;i--){
-									if(elevator.queue[i]){
-										elevator.direction=DIRN_DOWN;
-										break;
-									}
-									else
-										direction_next=DIRN_DOWN;
-								}
-								break;
-						case 0:
-								for(int i=0;i<N_FLOORS;i++){
-									if(elevator.queue[i]){
-										elevator.direction=DIRN_UP;
-										break;
-										}
-									else 
-										direction_next=DIRN_UP;
-								}
-								break;
-								
-						default:	
-							
-							if(elev_orders[elevator.floor_current][2] && my_ID);
-								up=1;
-							if(elev_orders[elevator.floor_current][2] && my_ID);	
-								down=1;
-							delta=last_floor-elevator.floor_current;
-							if((delta<0) && (up==1)){ //DIRN UP
-								for(int i=elevator.floor_current;i<N_FLOORS;i++){
-									if(elevator.queue[i]){
-										elevator.direction=DIRN_UP;
-										break;
-									}
-									else
-										direction_next=DIRN_UP;
-								}
-							}
-							else if((delta>0) && (down==1)){
-								for(int i=elevator.floor_current;i>=0;i--){
-									if(elevator.queue[i]){
-										elevator.direction=DIRN_DOWN;
-										break;
-									}
-									else
-										direction_next=DIRN_DOWN;
-								}
-							}
-							else if(up){
-								for(int i=elevator.floor_current;i<N_FLOORS;i++){
-									if(elevator.queue[i]){
-										elevator.direction=DIRN_UP;
-										break;
-									}
-									else
-										direction_next=DIRN_UP;
-								}
-							}
-							else if(down){
-								for(int i=elevator.floor_current;i>=0;i--){
-									if(elevator.queue[i]){
-										elevator.direction=DIRN_DOWN;
-										break;
-									}
-									else
-										direction_next=DIRN_DOWN;
-								}		
-							}
-						}
-					}
+void *tcp_send(void *transmit){
+	char *buf = (char*)transmit;
+   	char msg[BUFSIZE];
+	bzero(msg,BUFSIZE);
+	for(int i = 0;i<BUFSIZE;i++){
+		msg[i] = buf[i];
+	} 
+	if(network)
+    	write(*master_socket, msg, strlen(msg));
+}
 
+void tcp_recieve(void *socket_desc){
+  //Get the socket descriptor
+  int sock = *(int*)socket_desc;
+  //Set up timed socket
+  struct timeval tv;
+  tv.tv_sec = 11;  /* 2 Secs Timeout */
+  tv.tv_usec = 0;
+  setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO,(char *)&tv,sizeof(tv)); 
+  char buf[BUFSIZE];
+  char empty[BUFSIZE];
+  bzero(empty, BUFSIZE);
+  bzero(buf, BUFSIZE);
+  while(connection){
+    if(connection)
+    	recv(sock, buf , BUFSIZE,0);
+    if(!strcmp(buf,empty)){
+        puts("recv failed");
+        connection = 0;
+      }
+    else{
+     	char msg[BUFSIZE];
+      	strcpy(msg,buf);
+    	printf("Server message:\t");
+    	for(int i = 0;i<BUFSIZE;i++){
+	        if(buf[i] == '0'){
+	          buf[i] = 0;
+	      	}
+  			printf("%d ",buf[i]);
+  		 }
+		printf("\n");
+		strcpy(msg,buf);
+		pthread_t client_message_handler_thread;
+	    pthread_create(&client_message_handler_thread, NULL , client_message_handler, (void*)msg);
+	  }
+    bzero(buf, BUFSIZE);
+  }
+  puts("TCP Recieve Thread Killed");
+  free(socket_desc);    
+}
 
+void check_network(){
+	while(!network){
+		int fd;
+	    struct ifreq ifr;
+	    fd = socket(AF_INET, SOCK_DGRAM, 0);
+	    // Get IPv4 address attached to "eth0" 
+	    ifr.ifr_addr.sa_family = AF_INET;
+	    strncpy(ifr.ifr_name, "eth0", IFNAMSIZ-1);
+	    ioctl(fd, SIOCGIFBRDADDR, &ifr);
+	    close(fd);
 
-
+	    char* no_network;
+	    no_network = "0.0.0.0";
+	    char* BROADCASTIP = inet_ntoa(((struct sockaddr_in *)&ifr.ifr_broadaddr)->sin_addr);
+	    printf("%s\n",BROADCASTIP);
+	    if(strcmp(no_network,BROADCASTIP)){
+	    	network = 1;
+	    	connection = 0;
+	    }
+	    sleep(10);
+	}
+}
 
